f0d3d8431de19a65d085c34de43b3c094a760486
LOUIS TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGLO2@OOCL.COM>
2019-07-16 17:51:58 +0800

    complete:all

---
+++ src/test/java/com/tw/apistackbase/controller/EmployeesControllerTest.java
@@ -0,0 +1,16 @@
+package com.tw.apistackbase.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeesControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+
+
+}
